15492d95f805b208061b142b7bdd723ce65ec6d9
Kevin Keylock <kevinkeylock@hotmail.com>
2024-01-28 12:15:32 -0800

    Create a function to create divs

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const container = document.querySelector('.container')
 
+
+function createGrid (num){
+  for (let i = 0; i < num; i++){
+  const i = document.createElement("div");
+  i.classList.add('div');
+  container.appendChild(i);
+}
+}
+
+
+createGrid(16)

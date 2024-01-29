61a3e45a0786f10ad9fe12a4ffae5f972b1585a3
Kevin Keylock <kevinkeylock@hotmail.com>
2024-01-28 18:23:42 -0800

    Create function for rows, columns with cursor over
    Add prompt for amount of columns and rows

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const container = document.querySelector('.container')
+const row = prompt("Please enter amount of rows!");
+const column = prompt("Please enter number of columns!");
 
-
-function createGrid (num){
-  for (let i = 0; i < num; i++){
-  const i = document.createElement("div");
-  i.classList.add('div');
-  container.appendChild(i);
+function createGrid (num1, num2){
+  for (let i = 0; i < num1; i++){
+    const div = document.createElement("div");
+    div.classList.add('div');
+    container.appendChild(div);
+  for (let i = 0; i < num2; i++){
+    const area = document.createElement("div");
+    area.classList.add('area');
+    div.appendChild(area);
+    area.addEventListener("mouseover", function() {
+      area.style.backgroundColor = "red";
+    })
+  }}
 }
-}
+
+createGrid(row, column)
 
 
-createGrid(16)
+
+

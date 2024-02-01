a95352880269e5726bef46c9daa3dbf543261dbb
Kevin Keylock <kevinkeylock@hotmail.com>
2024-02-01 11:35:26 -0800

    Add buttons to change colors

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const container = document.querySelector('.container');
 const choose = document.querySelector('#choose');
-const row = prompt("Enter number of blocks per row");
+const color = document.querySelector('#color');
+const div = document.querySelector(".div");
+const area = document.querySelector(".area");
+
+const row = prompt("Enter number of pixels per row");
 const column = row;
 
 if (row > 100){
@@ -10,19 +14,14 @@ else {
   createGrid(row, column);
 }
 
-choose.addEventListener("click", function() {
-  location.reload()
-})
-
 function createGrid (num1, num2){
   for (let i = 0; i < num1; i++){
     const div = document.createElement("div");
-    div.classList.add('div');
+    //div.classList.add('div');
     container.appendChild(div);    
-
   for (let i = 0; i < num2; i++){
     const area = document.createElement("div");
-    area.classList.add('area');
+    //area.classList.add('area');
     div.appendChild(area);
     const height = 600 / num1 - 2;
     area.style.height = height + "px";
@@ -31,7 +30,52 @@ function createGrid (num1, num2){
     area.addEventListener("mouseover", function() {
       area.style.backgroundColor = "red";
     })
-  }}
-}
+  clear.addEventListener("click", function() {
+    area.style.backgroundColor = "white";
+  })
+  yellow.addEventListener("click", function() {
+    area.addEventListener("mouseover", function() {
+      area.style.backgroundColor = "yellow";
+  })})
+  white.addEventListener("click", function() {
+    area.addEventListener("mouseover", function() {
+      area.style.backgroundColor = "white";
+  })})
+  red.addEventListener("click", function() {
+    area.addEventListener("mouseover", function() {
+      area.style.backgroundColor = "red";
+  })})
+  blue.addEventListener("click", function() {
+    area.addEventListener("mouseover", function() {
+      area.style.backgroundColor = "blue";
+  })})
+  green.addEventListener("click", function() {
+    area.addEventListener("mouseover", function() {
+      area.style.backgroundColor = "green";
+  })})
+  black.addEventListener("click", function() {
+    area.addEventListener("mouseover", function() {
+      area.style.backgroundColor = "black";
+  })})
+  random.addEventListener("click", function() {
+    area.addEventListener("mouseover", function() {
+      const randomColor = Math.floor(Math.random()*16777215).toString(16);
+      area.style.backgroundColor = "#" + randomColor;
+  })})
+  pencil.addEventListener("click", function() {
+      let mouseOverCount = 0;
+    area.addEventListener("mouseover", function() {
+      mouseOverCount ++;
+      const darknessPercentage = (mouseOverCount / 10) * 100;
+      area.style.backgroundColor = `hsl(0, 0%, ${100 - darknessPercentage}%)`;
+  })})
+
+
+}}}
+
+
+choose.addEventListener("click", function() {
+  location.reload();
+})
 
 
